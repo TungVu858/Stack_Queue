93a2466aee4fa6797dcf89c0400281ac37883478
TungVu858 <tungvt858@gmail.com>
2022-04-19 10:47:37 +0700

    1

---
+++ src/baitap/Abc.java
@@ -0,0 +1,35 @@
+package baitap;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Abc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập n phần tử vào mảng : ");
+        System.out.print("n = ");
+        int n = scanner.nextInt();
+        String s = "";
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            stack.push(scanner.nextInt());
+        }
+        System.out.println(stack);
+        for (int i = 0; i < n; i++) {
+            stack2.push(stack.pop());
+        }
+        System.out.println(stack2);
+        String chuoi = "hello";
+        Stack<Character> stack1 = new Stack<>();
+        Stack<Character> stack3 = new Stack<>();
+        for (int i = 0; i < chuoi.length(); i++) {
+            stack1.push(chuoi.charAt(i));
+        }
+        System.out.println(stack1);
+        while (!stack1.isEmpty()){
+            stack3.push(stack1.pop());
+        }
+        System.out.println(stack3);
+    }
+}
